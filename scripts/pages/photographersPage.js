214ef1e3fe93cbf0ac49612b1441ca237b84774c
Gareth Thomas <thomasgareth3@gmail.com>
2023-08-14 23:12:37 +0200

    feat:lightbox

--- scripts/pages/photographersPage.js
+++ scripts/pages/photographersPage.js
@@ -45,6 +45,9 @@ async function displayData(photographers, media) {
   likeCardDOM.forEach((element) => {
     likeSection.appendChild(element);
   });
+
+  findIndexOfClickedMedia(filteredMedia);
+  initializeSlideListeners();
 }
 
 async function init() {
@@ -71,23 +74,48 @@ function filterMedia(data, id) {
 }
 
 // CAROUSSEL //
-let slideIndex = 1;
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+
+let currentSlideIndex = 0; // Global variable to keep track of the slide
+
+function findIndexOfClickedMedia(filteredMedia) {
+  let allMedia = document.querySelectorAll(".media");
+
+  allMedia.forEach((media) => {
+    media.addEventListener("click", function () {
+      let idOfClickedImage = this.getAttribute("data-id");
+      currentSlideIndex = filteredMedia.findIndex(
+        (someMedia) => someMedia.id === parseInt(idOfClickedImage)
+      );
+      showSlides(currentSlideIndex);
+    });
+  });
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("lightbox-media");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
+function showSlides(slideIndex) {
+  let modal = document.querySelector("#lightbox-modal");
+  let slides = document.getElementsByClassName("lightbox-media");
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex - 1].style.display = "block";
+  modal.style.display = "block";
+  slides[slideIndex].style.display = "block";
+}
+
+// Initialize 2 event listeners for each of the arrow keys on the slides
+function initializeSlideListeners() {
+  //each one of these checks if current slide is the first or last slide, then adds or takes 1 away from the index if they aren't
+  document.querySelector(".prev").addEventListener("click", () => {
+    if (currentSlideIndex > 0) {
+      currentSlideIndex--;
+      showSlides(currentSlideIndex);
+    }
+  });
+
+  document.querySelector(".next").addEventListener("click", () => {
+    let slides = document.getElementsByClassName("lightbox-media");
+    if (currentSlideIndex < slides.length - 1) {
+      currentSlideIndex++;
+      showSlides(currentSlideIndex);
+    }
+  });
 }
